ecfc616d66f6331697cf9a6c474792c3cc30d990
salmannaeem112215 <salman.naeem.112215@gmail.com>
2023-06-27 18:48:59 +0500

    App Build

--- lib/modules/home/view/body.dart
+++ lib/modules/home/view/body.dart
@@ -8,77 +8,78 @@ class Body extends StatelessWidget {
     final dc = Get.find<DataController>();
 
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
+      padding: const EdgeInsets.symmetric(horizontal: 8),
       child: Column(
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           const SizedBox(height: 8),
-          Row(
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Expanded(
-                child: Obx(() {
-                  dc.datas.length;
-                  return CardTile(
-                    icon: IconConfig.device,
-                    bgColor: ColorConfig.cardColorLight,
-                    isDark: dc.deviceActive,
-                    canTap: false,
-                    subText: 'Device',
-                    textDark: 'Active',
-                    textLight: 'Offline',
-                  );
-                }),
+          SizedBox(
+            height: 350,
+            child: GridView(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
               ),
-              const SizedBox(width: 8),
-              Expanded(
-                child: Obx(() {
-                  dc.datas.length;
-                  return CardTile(
-                    icon: IconConfig.temperature,
-                    bgColor: ColorConfig.cardColorDark,
-                    subText: 'Temp- C',
-                    isDark: true.obs,
-                    textDark: dc.latestTemp.toStringAsFixed(1),
-                    textLight: dc.avgTemp.toStringAsFixed(1),
-                  );
-                }),
-              ),
-            ],
-          ),
-          const SizedBox(height: 8),
-          Row(
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Expanded(
-                child: Obx(() {
-                  dc.datas.length;
-                  return CardTile(
-                    icon: IconConfig.pressure,
-                    bgColor: ColorConfig.cardColorDark,
-                    subText: 'Pressure- Pa',
-                    isDark: true.obs,
-                    textDark: dc.latestPress.toStringAsFixed(1),
-                    textLight: dc.avgPress.toStringAsFixed(1),
-                  );
-                }),
-              ),
-              const SizedBox(width: 8),
-              Expanded(
-                child: Obx(() {
-                  dc.datas.length;
-                  return CardTile(
-                    icon: IconConfig.humidity,
-                    bgColor: ColorConfig.cardColorDark,
-                    isDark: true.obs,
-                    subText: 'Humidity - %',
-                    textDark: dc.latestHumidity.toStringAsFixed(1),
-                    textLight: dc.avgHumidity.toStringAsFixed(1),
-                  );
-                }),
-              ),
-            ],
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 3, bottom: 3),
+                  child: Obx(() {
+                    dc.datas.length;
+                    return CardTile(
+                      icon: IconConfig.device,
+                      bgColor: ColorConfig.cardColorLight,
+                      isDark: dc.deviceActive,
+                      canTap: false,
+                      subText: 'Device',
+                      textDark: 'Active',
+                      textLight: 'Offline',
+                    );
+                  }),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 3, bottom: 3),
+                  child: Obx(() {
+                    dc.datas.length;
+                    return CardTile(
+                      icon: IconConfig.temperature,
+                      bgColor: ColorConfig.cardColorDark,
+                      subText: "Temp C°",
+                      isDark: true.obs,
+                      textDark: dc.latestTemp.toStringAsFixed(1),
+                      textLight: dc.avgTemp.toStringAsFixed(1),
+                    );
+                  }),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 3, top: 3),
+                  child: Obx(() {
+                    dc.datas.length;
+                    return CardTile(
+                      icon: IconConfig.pressure,
+                      bgColor: ColorConfig.cardColorDark,
+                      subText: 'Pressure Pa',
+                      isDark: true.obs,
+                      textDark: dc.latestPress.toStringAsFixed(1),
+                      textLight: dc.avgPress.toStringAsFixed(1),
+                    );
+                  }),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 3, top: 3),
+                  child: Obx(() {
+                    dc.datas.length;
+                    return CardTile(
+                      icon: IconConfig.humidity,
+                      bgColor: ColorConfig.cardColorDark,
+                      isDark: true.obs,
+                      subText: 'Humidity %',
+                      textDark: dc.latestHumidity.toStringAsFixed(1),
+                      textLight: dc.avgHumidity.toStringAsFixed(1),
+                    );
+                  }),
+                ),
+              ],
+            ),
           ),
           const SizedBox(height: 8),
         ],
@@ -121,51 +122,50 @@ class CardTile extends StatelessWidget {
             mainAxisSize: MainAxisSize.max,
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Expanded(
-                child: Image.asset(
-                  icon,
-                  height: 75,
-                ),
+              Image.asset(
+                icon,
+                height: 50,
               ),
-              const SizedBox(width: 8),
-              Expanded(
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      canTap
-                          ? Obx(
-                              () => Text(
-                                isDark.value ? 'Latest' : 'Avg',
-                                textAlign: TextAlign.center,
-                                style: const TextStyle(
-                                  fontSize: 16,
-                                  color: Colors.white70,
-                                ),
+              const SizedBox(width: 2),
+              Flexible(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    canTap
+                        ? Obx(
+                            () => Text(
+                              isDark.value ? 'Latest' : 'Avg',
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(
+                                fontSize: 16,
+                                overflow: TextOverflow.ellipsis,
+                                color: Colors.white70,
                               ),
-                            )
-                          : const SizedBox.shrink(),
-                      Obx(
-                        () => Text(
-                          isDark.value ? textDark : textLight,
-                          textAlign: TextAlign.center,
-                          style: const TextStyle(
-                            fontSize: 22,
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 5),
-                      Text(
-                        subText,
+                            ),
+                          )
+                        : const SizedBox.shrink(),
+                    Obx(
+                      () => Text(
+                        isDark.value ? textDark : textLight,
                         textAlign: TextAlign.center,
+                        overflow: TextOverflow.ellipsis,
                         style: const TextStyle(
-                          fontSize: 14,
-                          color: Colors.white70,
+                          fontSize: 22,
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                    const SizedBox(height: 5),
+                    Text(
+                      subText,
+                      textAlign: TextAlign.center,
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 2,
+                      style: const TextStyle(
+                        fontSize: 14,
+                        color: Colors.white70,
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
